8c8039f72a8fdb5ef084b8f16a810541f96c7568
lukevangorder <lukevangorder@gmail.com>
2021-05-06 20:59:40 -0400

    commit tree 329cb4fd78c716e24882d26b5237501d6c5e3a69

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    return sourceArray.map( function(int) {
+        return int * -1;
+    });
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray
+}
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map( function(int) {
+        return int * 2;
+    });
+}
+
+function mapToSquare(sourceArray) {
+    return sourceArray.map( function(int) {
+        return int ** 2;
+    });
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+    return sourceArray.reduce( function(total, int) {
+        return int + total;
+    }, startingPoint);
+}
+
+function reduceToAllTrue(sourceArray) {
+    return sourceArray.reduce( function(total, int) {
+        return !!int;
+    }, false);
+}
+
+function reduceToAnyTrue(sourceArray) {
+    return sourceArray.reduce( function(total, int) {
+        if (int === true) {
+            return true;
+        } else {
+            if (!total) {
+                return false;
+            }
+        }
+    }, false);
+}
